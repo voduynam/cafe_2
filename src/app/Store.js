fb0ea1db7d2aa44398ffa482e61b1c6b8aadd480
voduynam <voduy1176@gmail.com>
2024-08-01 18:06:36 +0700

    hihihi

--- src/app/Store.js
+++ src/app/Store.js
@@ -2,14 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import itemsReducer from '../redux/mallSlice'; 
 import chooseMallReducer from '../redux/handleChooseMall'; 
 import cartReducer from '../redux/cartSlice'; 
-import paymentReducer from '../redux/paymentSlice'; 
+
 
 const store = configureStore({
   reducer: {
     items: itemsReducer,
     mall: chooseMallReducer,
     cart: cartReducer,
-    payment: paymentReducer,
+    
   },
 });
 
---
+++ src/redux/paymentSlice.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const paymentSlice = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default paymentSlice
